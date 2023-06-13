93e77945f58eeab6af5847c92154fd293bb61e30
Maxim Baz <git@maximbaz.com>
2023-06-14 00:41:30 +0200

    Add framer staging

--- src/helpers/cors.ts
+++ src/helpers/cors.ts
@@ -3,6 +3,9 @@ import util from "util";
 
 export const cors = util.promisify(
   Cors({
-    origin: ["https://giveffektivt.dk"],
+    origin: [
+      "https://giveffektivt.dk",
+      "https://glad-function-083442.framer.app",
+    ],
   })
 );
