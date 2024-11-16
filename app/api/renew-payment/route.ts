3013755bfc800716bc6b4f0f04c32867ef4baf7f
Maxim Baz <git@maximbaz.com>
2024-11-16 22:24:00 +0100

    /api/renew-payment: migrate to app router

---
+++ app/api/renew-payment/route.ts
@@ -0,0 +1,44 @@
+import {
+  getRenewPaymentLink,
+  type SubmitDataRenewPayment,
+  validationSchemaRenewPayment,
+} from "src";
+import * as yup from "yup";
+
+export async function POST(req: Request) {
+  try {
+    let submitData: SubmitDataRenewPayment | null = null;
+    try {
+      submitData = await yup
+        .object()
+        .shape(validationSchemaRenewPayment)
+        .validate(req.body);
+    } catch (err) {
+      if (err instanceof yup.ValidationError) {
+        console.error(
+          "api/renew-payment: Validation failed for request body: ",
+          err,
+        );
+        return Response.json(
+          { message: "Validation failed", errors: err.errors },
+          { status: 400 },
+        );
+      }
+      throw err;
+    }
+
+    const url = await getRenewPaymentLink(submitData.id);
+
+    if (url == null) {
+      console.error(
+        `api/renew-payment: attempted to renew payment on donation '${submitData.id}' that doesn't allow it`,
+      );
+      return Response.json({ message: "Not found" }, { status: 404 });
+    }
+
+    return Response.json({ message: "OK", url });
+  } catch (err) {
+    console.error("api/renew-payment:", err);
+    return Response.json({ message: "Something went wrong" }, { status: 500 });
+  }
+}
