88c8b5965e91259e4b9770fa19b741b53ed00345
Maxim Baz <git@maximbaz.com>
2024-11-16 23:06:03 +0100

    api/failed-recurring-donations: migrate to app router

---
+++ app/api/failed-recurring-donations/route.ts
@@ -0,0 +1,26 @@
+import { dbExecuteInTransaction } from "src";
+import { sendFailedRecurringDonationEmails } from "src/donation/failed";
+
+export async function POST(req: Request) {
+  try {
+    if (!process.env.FAILED_RECURRING_DONATIONS_API_KEY) {
+      throw new Error("FAILED_RECURRING_DONATIONS_API_KEY is not defined");
+    }
+
+    if (
+      req.headers.get("Authorization") !==
+      `Bearer ${process.env.FAILED_RECURRING_DONATIONS_API_KEY}`
+    ) {
+      return Response.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    await dbExecuteInTransaction(async (db) => {
+      await sendFailedRecurringDonationEmails(db, await req.json());
+    });
+
+    return Response.json({ message: "OK" });
+  } catch (err) {
+    console.error("api/failed-recurring-donations:", err);
+    return Response.json({ message: "Something went wrong" }, { status: 500 });
+  }
+}
