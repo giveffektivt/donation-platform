9487c38f7e7207792b3050c059f638b88b9637b2
Maxim Baz <git@maximbaz.com>
2024-11-21 22:07:58 +0100

    try cors using old express library with middleware

---
+++ middleware.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import Cors from "cors";
+
+export async function middleware(request: NextRequest) {
+  const headers = await executeCors(request);
+  const response = NextResponse.next();
+  for (const [key, value] of headers.entries()) {
+    response.headers.set(key, value);
+  }
+  return response;
+}
+
+async function executeCors(request: NextRequest): Promise<Headers> {
+  const mockRequest = {
+    method: request.method,
+    headers: Object.fromEntries(request.headers.entries()),
+  };
+
+  return new Promise((resolve, reject) => {
+    const headers = new Headers();
+    const mockResponse = {
+      getHeader: (key: string) => headers.get(key),
+      setHeader: (key: string, value: string) => headers.set(key, value),
+      end: () => resolve(headers),
+    };
+
+    Cors({
+      origin: [
+        "https://giveffektivt.dk",
+        ...(process.env.DEV_WEBSITE_DOMAINS
+          ? process.env.DEV_WEBSITE_DOMAINS.split(",")
+          : []),
+      ],
+    })(mockRequest, mockResponse, (error) =>
+      error ? reject(error) : resolve(headers),
+    );
+  });
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
