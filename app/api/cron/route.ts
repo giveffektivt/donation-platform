b69762bf289715b9162deb13e06af6f826044996
Maxim Baz <git@maximbaz.com>
2024-12-06 14:02:38 +0100

    debug cron

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,7 +9,16 @@ export async function POST(req: Request) {
     if (
       req.headers.get("Authorization") !== `Bearer ${process.env.CRON_API_KEY}`
     ) {
-      logError("api/cron: Unauthorized");
+      logError("api/cron: Unauthorized", {
+        req: {
+          len: req.headers.get("Authorization")?.length,
+          pref: req.headers.get("Authorization")?.substring(0, 3),
+        },
+        env: {
+          len: process.env.CRON_API_KEY?.length,
+          pref: process.env.CRON_API_KEY?.substring(0, 3),
+        },
+      });
       return Response.json({ message: "Unauthorized" }, { status: 401 });
     }
 
