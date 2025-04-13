6166f8b6f85423e94fd48a71dbad6aca6c2c31a9
Maxim Baz <git@maximbaz.com>
2025-04-13 10:36:10 +0200

    stop hubspot export

--- src/helpers/crm.ts
+++ src/helpers/crm.ts
@@ -3,41 +3,7 @@ import { type CrmExport, getCrmExport } from "src";
 
 export async function ExportToCrm(db: PoolClient) {
   const entries = await getCrmExport(db);
-  exportToHubspot(entries);
   exportToBrevo(entries);
-}
-
-async function exportToHubspot(entries: CrmExport[]) {
-  const toDate = (date: Date | null) => date?.toISOString()?.split("T")[0];
-  const payloads = entries.map((entry) => ({
-    idProperty: "email",
-    id: entry.email,
-    properties: {
-      ...(entry.name ? { firstname: entry.name } : {}),
-      oprettelsesdato: toDate(entry.registered_at),
-      email: entry.email,
-      sidste_donationsdato: toDate(entry.last_donated_at),
-      sidste_donationsbelob: entry.last_donated_amount,
-      sidste_donationsfrekvens: entry.last_donated_frequency,
-      sidste_donationsmetode: entry.last_donated_method,
-      er_sidste_donation_opsagt: entry.last_donation_cancelled,
-      er_sidste_donation_fradragsberettiget: entry.last_donation_tax_deductible,
-      sidste_donationsoeremaerkning: entry.last_donated_recipient,
-      totalt_doneret: entry.total_donated,
-      antal_donationer: entry.donations_count,
-      medlem: entry.is_member,
-    },
-  }));
-
-  const batchSize = 100;
-  const totalBatches = Math.ceil(entries.length / batchSize);
-
-  const batchPromises = Array.from({ length: totalBatches }, (_, i) => {
-    const batch = payloads.slice(i * batchSize, (i + 1) * batchSize);
-    return uploadBatchToHubspot(batch);
-  });
-
-  await Promise.all(batchPromises);
 }
 
 async function exportToBrevo(entries: CrmExport[]) {
@@ -62,24 +28,6 @@ async function exportToBrevo(entries: CrmExport[]) {
   }));
 
   await uploadBatchToBrevo(payloads);
-}
-
-async function uploadBatchToHubspot(batch: object[]) {
-  const response = await fetch(
-    `${process.env.HUBSPOT_API_URL}/crm/v3/objects/contacts/batch/upsert`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ inputs: batch }),
-    },
-  );
-
-  if (!response.ok) {
-    throw await response.text();
-  }
 }
 
 async function uploadBatchToBrevo(batch: object[]) {
