63a7a956810d40039bbd879782e95e2a3dba0f62
Maxim Baz <git@maximbaz.com>
2025-06-12 22:23:35 +0200

    rollbar: disable capturing unhandled rejections

--- src/helpers/rollbar.ts
+++ src/helpers/rollbar.ts
@@ -3,7 +3,7 @@ import Rollbar from "rollbar";
 const rollbar = new Rollbar({
   accessToken: process.env.ROLLBAR_SERVER_TOKEN,
   captureUncaught: true,
-  captureUnhandledRejections: true,
+  captureUnhandledRejections: false,
   environment: process.env.VERCEL_ENV,
   captureIp: false,
   transform: (payload: any) => {
